0062b376715b2c1ab875d3a8579b4eb97c74c92f
Ncosta09 <ncosta981@gmail.com>
2023-10-16 10:30:41 -0300

    finalizado preview de perfil de usuario

--- src/components/Usuario/Perfl_Usuario/usuarioPerfil.js
+++ src/components/Usuario/Perfl_Usuario/usuarioPerfil.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+
+import "../Perfl_Usuario/usuarioPerfil.css"
 
 function PerfilUsuario() {
     const { userId } = useParams();
@@ -14,18 +16,27 @@ function PerfilUsuario() {
     }, [userId]);
 
     return (
-        <div>
-            {usuario && (
-                <div>
-                    <h2>Perfil de Usuario</h2>
-                    <p>ID: {usuario.usuario.ID}</p>
-                    <p>Nombre: {usuario.usuario.Nombre}</p>
-                    <p>Apellido: {usuario.usuario.Apellido}</p>
-                    <p>Email: {usuario.usuario.Email}</p>
-                    <p>Teléfono: {usuario.usuario.Telefono}</p>
-                    <img src = {usuario.usuario.Imagen} alt = {usuario.usuario.Imagen} />
-                </div>
-            )}
+        <div className='main'>
+            <div className='main-profile'>
+                <h2>Perfil de Usuario</h2>
+                {usuario && (
+                    <div className='main-info'>
+                        <div className='info-box'>
+                            <div className='first-info'>
+                                <p>ID: {usuario.usuario.ID}</p>
+                                <p>Nombre: {usuario.usuario.Nombre}</p>
+                                <p>Apellido: {usuario.usuario.Apellido}</p>
+                                <p>Email: {usuario.usuario.Email}</p>
+                                <p>Teléfono: {usuario.usuario.Telefono}</p>
+                                <p>Fecha Creación: {new Date(usuario.usuario.Fecha_Creacion).toLocaleString()}</p>
+                            </div>
+                            <img src = {usuario.usuario.Imagen} alt = {usuario.usuario.Imagen} />
+                        </div>
+                    </div>
+                )}
+
+                <Link to='/usuarios' className='goBack'>Vovler</Link>
+            </div>
         </div>
     );
 }
