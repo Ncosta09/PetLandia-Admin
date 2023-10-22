1554dddf0b97ebc130b22b1a7a2e6e454e201112
Ncosta09 <ncosta981@gmail.com>
2023-10-22 01:06:02 -0300

    agregado de pestaña de detalle de venta con redireccion a cada producto

---
+++ src/components/Venta/Perfil_Venta/ventaPerfil.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import "../Perfil_Venta/ventaPerfil.css"
+
+function PerfilVenta() {
+    const { ventaId } = useParams();
+    const [venta, setVenta] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://petlandia.onrender.com/api/busquedaVentas/${ventaId}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setVenta(data);
+            });
+    }, [ventaId]);
+
+    const datosVenta = () => {
+        if (venta && venta.data) {
+            const datosVenta = venta.data;
+
+            return (
+                <table className="tabla">
+                    <thead>
+                        <tr className='listado-info'>
+                            <th>ID de Venta</th>
+                            <th>Usuario</th>
+                            <th>Total de la Venta</th>
+                            <th>Costo de Envío</th>
+                            <th>Fecha</th>
+                        </tr>
+                    </thead>    
+                    <tbody>
+                        <tr className='listado-info'>
+                            <td>{datosVenta.ID}</td>
+                            <td>{datosVenta.Usuarios.Nombre} {datosVenta.Usuarios.Apellido}</td>
+                            <td>${datosVenta.Precio_Total}</td>
+                            <td>${datosVenta.Costo_Envio}</td>
+                            <td>{new Date(datosVenta.Fecha).toLocaleDateString()}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            );
+        }
+    };
+
+    const detalleVenta = () => {
+        if (venta && venta.data && venta.data.DetalleVentas && venta.data.DetalleVentas.length > 0) {
+            return (
+                <table className="tabla">
+                    <thead>
+                        <tr className='listado-info'>
+                            <th>ID</th>
+                            <th>Precio Unidad</th>
+                            <th>Cantidad</th>
+                            <th>Descuento</th>
+                            <th>Envío</th>
+                            <th>Producto</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {venta.data.DetalleVentas.map((detalle, index) => (
+                            <tr key={index} className='listado-info'>
+                                <td>{detalle.ID}</td>
+                                <td>${detalle.Precio_Unidad}</td>
+                                <td>{detalle.Cantidad}</td>
+                                <td>{detalle.Descuento}%</td>
+                                <td>${detalle.Envio}</td>
+                                <td><Link to={`/producto/${detalle.ID}`}>Ver producto</Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            );
+        }
+    };
+
+    return (
+        <div>
+            {datosVenta()}
+            {detalleVenta()}
+        </div>
+    );
+}
+
+export default PerfilVenta;
